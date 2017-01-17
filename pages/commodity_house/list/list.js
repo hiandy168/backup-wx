ba4609f81638e43f13830b15fcf915b5af6fceb5
wujunchao_work <zjuwujunchao@gmail.com>
2017-01-17 15:49:17 +0800

    商品房列表页

--- pages/commodity_house/list/list.js
+++ pages/commodity_house/list/list.js
@@ -2,16 +2,81 @@
 var app = getApp();
 
 Page({
+  data: {
+    cityFilter: {
+      current: 1
+    },
+    categoryFilter: {
+      current: 0
+    },
+    priceRangeFilter: {
+      current: 0
+    },
+    decorationStandardFilter: {
+      current: 0
+    }, 
+    roomTypesFilter: {
+      current: 0
+    }, 
+    stylesFilter: {
+      current: 0
+    }, 
+    isFilterExpanded: false
+  },
+
+  bindFilterToggle()
+  {
+    this.setData({
+      isFilterExpanded: !this.data.isFilterExpanded
+    })
+  },
+
+  toCommodityHouseIndex: function (event) {
+    wx.navigateTo({
+      url: '../index/index?id=' + event.target.id
+    })
+  },
   onLoad: function () {
     var self = this
+    wx.request({
+      url: app.globalData.siteUrl + '/api/commodity-house/get-filter',
+      data: {},
+      method: 'GET',
+      success: function (res) {
+        // console.log(res.data.data)
+        self.setData({ 
+          cityFilter: {
+            current: self.data.cityFilter.current,
+            filterRange: res.data.data.cities
+          },
+          categoryFilter: {
+            current: self.data.categoryFilter.current,
+            filterRange: res.data.data.categories
+          },
+          priceRangeFilter: {
+            current: self.data.priceRangeFilter.current,
+            filterRange: res.data.data.price_ranges
+          },
+          decorationStandardFilter: {
+            current: self.data.decorationStandardFilter.current,
+            filterRange: res.data.data.decoration_standards
+          },
+          roomTypesFilter: {
+            current: self.data.roomTypesFilter.current,
+            filterRange: res.data.data.room_types
+          },
+          stylesFilter: {
+            current: self.data.stylesFilter.current,
+            filterRange: res.data.data.styles
+          }
+        });
+      },
+    })
     wx.request({
       url: app.globalData.siteUrl + '/api/commodity-house/get-list',
       data: {
         pagesize: 10,
         photo_cover_size: '300x300'
-      },
-      header: {
-        'content-type': 'application/json'
       },
       success: function (res) {
         console.log(res.data.data);
@@ -20,5 +85,9 @@ Page({
         })
       }
     })
+  },
+
+  filterChange() {
+    alert(1);
   }
 })
