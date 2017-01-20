e56280896c342cc56d93ef18676add6b7cb63807
ylx <6@nahaowan.com>
2017-01-20 17:10:23 +0800

    商品房详情页

--- pages/commodity_house/index/index.js
+++ pages/commodity_house/index/index.js
@@ -1,19 +1,34 @@
+var WxParse = require('../../../utils/wxParse/wxParse.js');
 var app = getApp();
 
 Page({
   data: {
-    houseMetaStatus: 0
+    houseMetaStatus: 0,
+    id:0,
+    photo_index: 1,
+    show_detail: false,
     //   house: {
     //     name: '楼盘名称'
     //   }
   },
 
+  photochange: function(e){
+    this.setData({
+      photo_index:e.detail.current+1
+    });
+  },
   onLoad: function (options) {
     this.setData({
       id: options.id
     })
-
+    wx.showToast({
+        title: '载入中',
+        icon: 'loading',
+        duration: 60000
+      })
     var that = this
+    var test = '<div>我是HTML代码</div>';
+    WxParse.wxParse('description', 'html', test , that,0);
     wx.request({
       url: app.globalData.siteUrl + '/api/commodity-house/index',
       data: {
@@ -26,17 +41,27 @@ Page({
   //       console.log(res.data.data)
         that.setData({
           house: res.data.data
-        })
+        });
+
+      },
+      complete: function() {
+        wx.hideToast();
       }
     })
   },
-
+  showmore: function(){
+    var show = this.data.show_detail;
+   
+    this.setData({
+      show_detail:!show,
+    });
+  },
   houseMetaShowDetail() {
     this.setData({
       houseMetaStatus: 1
     })
   },
-
+  
   houseMetaHideDetail() {
     this.setData({
       houseMetaStatus: 0
