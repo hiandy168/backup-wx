81cc55d0619be952d3f4c980999640569b6897b2
wujunchao_work <zjuwujunchao@gmail.com>
2017-02-07 17:15:30 +0800

    house list

--- pages/house/list/list.js
+++ pages/house/list/list.js
@@ -2,74 +2,68 @@
 var app = getApp();
 
 Page({
+
   data: {
+    house_list: [],
     currentCity: 0,
+    currentDistance: 0,
     currentCategory: 0,
     currentPriceRange: 0,
-    currentDecoration: 0,
-    currentRoom: 0,
-    currentStyle: 0,
-    page: 1,
+    currentResourceType: 0,
+    currentLandArea: 0,
+    page: 0,
     isFilterExpanded: false
   },
 
+  // 展开更多(切换)
   bindFilterToggle() {
-     this.setData({
+    this.setData({
       isFilterExpanded: !this.data.isFilterExpanded
     })
   },
 
-  toCommodityHouseIndex: function (e) {
+  // 前往index的链接
+  toHouseIndex: function (e) {
     wx.navigateTo({
       url: '../index/index?id=' + e.target.id
     })
   },
+
+  // 获取筛选条目, 获取房源列表
   onLoad: function (option) {
     var self = this
+
     wx.request({
-      url: app.globalData.siteUrl + '/api/commodity-house/get-filter',
+      url: app.globalData.siteUrl + '/api/house/get-filter',
       method: 'GET',
       success: function (res) {
 
-        // console.log(res.data.data)
+        console.log(res.data.data)
 
         self.setData({
           cityFilterRange: res.data.data.cities,
+          distanceFilterRange: res.data.data.distances,
           categoryFilterRange: res.data.data.categories,
           priceRangeFilterRange: res.data.data.price_ranges,
-          decorationFilterRange: res.data.data.decoration_standards,
-          roomFilterRange: res.data.data.room_types,
-          styleFilterRange: res.data.data.styles
+          resourceTypeFilterRange: res.data.data.resource_types,
+          landAreaFilterRange: res.data.data.land_areas,
         });
-        // 异步
-        wx.request({
-          url: app.globalData.siteUrl + '/api/house/get-list-wx',
-          data: {
-            region: self.data.cityFilterRange[[self.data.currentCity]].id,
-            cat: self.data.currentCategory,
-            price: self.data.currentPriceRange,
-            decoration: self.data.currentDecoration,
-            style: self.data.currentStyle,
-            pagesize: 10,
-            photo_cover_size: '300x300'
-          },
-          success: function (res) {
-
-            console.log(res.data.data);
-
-            self.setData({
-              house_list: res.data.data
-            })
-          }
-        });
+        // 异步, 先获取筛选条件, 再获取房源列表
+        self.getList()
       },
     })
-
   },
 
   bindCityChange: function (e) {
     this.setData({
       currentCity: e.detail.value
+    })
+    this.filterChanged()
+  },
+
+  bindDistanceChange: function (e) {
+    this.setData({
+      currentDistance: e.detail.value
     })
     this.filterChanged()
   },
@@ -88,103 +82,88 @@ Page({
     this.filterChanged()
   },
 
-  bindDecorationChange: function (e) {
+  bindResourceTypeChange: function (e) {
     this.setData({
-      currentDecoration: e.target.dataset.id
+      currentResourceType: e.detail.value
     })
+    this.filterChanged()
   },
 
-  bindRoomChange: function (e) {
+  bindLandAreaChange: function (e) {
     this.setData({
-      currentRoom: e.target.dataset.id
+      currentLandArea: e.detail.value
     })
-  },
-
-  bindStyleChange: function (e) {
-    this.setData({
-      currentStyle: e.target.dataset.id
-    })
+    this.filterChanged()
   },
 
   filterCancel() {
     this.setData({
       currentCity: 0,
+      currentDistance: 0,
       currentCategory: 0,
       currentPriceRange: 0,
-      currentDecoration: 0,
-      currentStyle: 0,
-      currentRoom: 0,
+      currentResourceType: 0,
+      currentLandArea: 0,
       isFilterExpanded: false
     });
     this.filterChanged()
   },
 
+  // 筛选条件改变, 刷新房源列表
   filterChanged() {
     this.setData({
-      isFilterExpanded: false
+      isFilterExpanded: false,
+      // 重置列表
+      page: 0
     })
-    var self = this
-    wx.showToast({
-      title: '载入中',
-      icon: 'loading',
-      duration: 60000
-    })
-    wx.request({
-      url: app.globalData.siteUrl + '/api/house/get-list-wx',
-      data: {
-        region: self.data.cityFilterRange[[self.data.currentCity]].id,
-        cat: self.data.currentCategory,
-        price: self.data.currentPriceRange,
-        decoration: self.data.currentDecoration,
-        style: self.data.currentStyle,
-        page: 1,
-        pagesize: 10,
-        photo_cover_size: '300x300'
-      },
-      success: function (res) {
-        // console.log(res.data.data);
-        var new_house_list = res.data.data
-        self.setData({
-          house_list: new_house_list,
-          page: self.data.page + 1
-        })
-      },
-      fail: function () {
-        console.log("获取数据失败");
-      },
-      complete: function () {
-        wx.hideToast();
-      }
-    })
+    this.getList()
   },
 
+  // 瀑布流
   getMore() {
+    this.getList();
+  },
+
+  // 获取房源列表, 包括初始列表和后续加载
+  getList() {
     var self = this
+
     wx.showToast({
       title: '载入中',
       icon: 'loading',
       duration: 60000
     })
-    var self = this;
+
     wx.request({
       url: app.globalData.siteUrl + '/api/house/get-list-wx',
       data: {
-        region: self.data.cityFilterRange[[self.data.currentCity]].id,
+        region: self.data.cityFilterRange[self.data.currentCity].id,
+        distance: self.data.distanceFilterRange[self.data.currentDistance].id,
         cat: self.data.currentCategory,
         price: self.data.currentPriceRange,
-        decoration: self.data.currentDecoration,
-        style: self.data.currentStyle,
+        land_area: self.data.currentLandArea,
+        resource_type: self.data.currentResourceType,
         page: self.data.page + 1,
         pagesize: 10,
         photo_cover_size: '300x300'
       },
       success: function (res) {
         // console.log(res.data.data);
-        var new_house_list = self.data.house_list.concat(res.data.data)
+
+        // 列表被重置
+        if (self.data.page == 0) {
+          var new_house_list = res.data.data
+        }
+        // 列表未重置, 添加到之前列表尾部
+        else {
+          var new_house_list = self.data.house_list.concat(res.data.data)
+        }
+
         self.setData({
           house_list: new_house_list,
           page: self.data.page + 1
         })
+
       },
       fail: function () {
         console.log("获取数据失败");
